0532b33a12a4be7d25dd1bd485aa68ee946ea67e
Saithirius <look1899@mail.ru>
2024-05-02 00:39:33 +0500

    upd Button install chromatic

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -4,20 +4,13 @@ import Button from './Button'
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
   component: Button,
-  parameters: {
-    layout: 'centered',
-  },
   tags: ['autodocs'],
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
-  // args: { onClick: fn() },
 }
 
 export default meta
 type Story = StoryObj<typeof Button>
 
-export const Normal: Story = {
+export const Simple: Story = {
   parameters: {
     docs: {
       description: {
@@ -33,5 +26,13 @@ export const Normal: Story = {
 export const Ghost: Story = {
   args: {
     children: 'Ghost',
+    type: 'ghost',
+  },
+}
+
+export const Text: Story = {
+  args: {
+    children: 'Ghost',
+    type: 'text',
   },
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import style from './Button.module.scss'
+import classNames from 'classnames'
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type Props = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type'> & {
   loading?: boolean
+  type?: 'default' | 'ghost' | 'text'
+  htmlType?: Pick<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type'>
 }
 
-const Button: React.FC<Props> = ({ children }) => {
-  return <button className={style.btn}>{children}</button>
+const Button: React.FC<Props> = ({ children, ...props }) => {
+  let typeClass = style.default
+  switch (props.type) {
+    case 'ghost':
+      typeClass = style.ghost
+      break
+    case 'text':
+      typeClass = style.text
+      break
+  }
+
+  return <button className={classNames([style.btn, typeClass])}>{children}</button>
 }
 
 export default Button
